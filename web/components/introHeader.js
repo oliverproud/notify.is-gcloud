b46b3dba675574851e293a61917865531bbf97e1
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-09 10:56:42 +0100

    remove backend testing, moved to main folder

--- web/components/introHeader.js
+++
@@ -1,7 +0,0 @@
-export default function IntroHeader({ children }) {
-  return (
-    <div className="intro-header px-4 py-5 pt-md-5 pb-md-5 mx-auto">
-      {children}
-    </div>
-  )
-}
--- web/services/rest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { catchAxiosError } from "./error";
-
-axios.defaults.baseURL = "http://localhost:1323"
-
-export const post = (url, data) => {
-  return axios.post(url, data).catch(catchAxiosError)
-}
