3bc605e1c8311fe7add1251f3c8f1b915383971f
Oliver Proud <oliverproud@users.noreply.github.com>
2020-07-27 16:02:23 +0100

    update twitter check

--- check/twitter.go
+++ check/twitter.go
@@ -8,19 +8,10 @@ import (
 	"github.com/dghubble/oauth1"
 )
 
-var username string
-
-func init() {
-	***REMOVED***("CONSUMER_KEY", "***REMOVED***")
-	***REMOVED***("CONSUMER_SECRET", "***REMOVED***")
-	***REMOVED***("TOKEN", "***REMOVED***")
-	***REMOVED***("TOKEN_SECRET", "***REMOVED***")
-
-	username = "oliverwproud"
-}
+var available bool
 
 // TwitterAPI uses a Golang Twitter API to make requests
-func TwitterAPI() {
+func TwitterAPI(username string) (bool, error) {
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
 	token := oauth1.NewToken(os.Getenv("TOKEN"), os.Getenv("TOKEN_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -32,17 +23,16 @@ func TwitterAPI() {
 	user, _, err := client.Users.Show(showParams)
 	if err != nil {
 		if err.Error() == "twitter: 50 User not found." {
-			// Do database stuff in here
-			fmt.Println(err)
+			available = true
+			fmt.Printf("Twitter: username %s available\n", username)
 		} else {
-			fmt.Println(err)
-			return
+			return available, err
 		}
 	}
 
 	if user.ID != 0 {
-		// This is where success email and database deletion would happen
-		fmt.Println("USER:", user.ID)
+		fmt.Printf("Twitter: username %s NOT available: ID %d\n", username, user.ID)
+		available = false
 	}
-
+	return available, nil
 }
