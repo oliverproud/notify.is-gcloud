1fc17d8ba91373e5334f46566fafb1a87484d89b
Oliver Proud <oliverproud@users.noreply.github.com>
2020-08-09 21:12:19 +0100

    update Instagram check to resolve false negative issues

--- check/instagram.go
+++ check/instagram.go
@@ -2,93 +2,72 @@ package check
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 )
 
-// InstagramResponse handles the XHR JSON data coming in from Instagram
-type InstagramResponse struct {
-	AccountCreated bool `json:"account_created"`
-	Errors         struct {
-		Username []struct {
-			Message string `json:"message"`
-			Code    string `json:"code"`
-		} `json:"username"`
-	} `json:"errors"`
-}
-
-// InstagramAvailable let's the main package know if a username is available
-var InstagramAvailable bool
-var parseInstagramResponse InstagramResponse
+// instagramAvailable let's the main package know if a username is available
+var instagramAvailable bool
+var nodes []*cdp.Node
 
 // Instagram runs the headless browser that checks Instagram
-func Instagram(username string) error {
+func Instagram(username string) (bool, error) {
 
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
+	// create chrome instance
+	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
 	defer cancel()
 
-	task, err := submit(ctx, `https://www.instagram.com/accounts/emailsignup/`, `//input[@name="username"]`, username)
+	// create a timeout
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	task, err := submit(`https://www.instagram.com/accounts/emailsignup/`, `//input[@name="username"]`, username)
 	if err != nil {
-		return err
+		return instagramAvailable, err
 	}
+
 	// run task
 	if err = chromedp.Run(ctx, task); err != nil {
-		return err
+		return instagramAvailable, err
 	}
-	return nil
+
+	if len(nodes) != 0 {
+		fmt.Printf("RESULT: %v\n", nodes[0])
+		fmt.Println("Sprite Type:", nodes[0].Attributes)
+		for i := range nodes[0].Attributes {
+			if strings.Contains(nodes[0].Attributes[i], "coreSpriteInputAccepted") {
+				instagramAvailable = true
+				fmt.Printf("Instagram: username %s is available\n", username)
+			} else if strings.Contains(nodes[0].Attributes[i], "coreSpriteInputError") {
+				instagramAvailable = false
+				fmt.Printf("Instagram: username %s is NOT available\n", username)
+			}
+		}
+	} else {
+		fmt.Println("Nothing returned")
+	}
+
+	return instagramAvailable, nil
 }
 
-func submit(ctx context.Context, urlstr, selector, username string) (chromedp.Tasks, error) {
-
-	chromedp.ListenTarget(ctx, func(event interface{}) {
-		if event, ok := event.(*network.EventResponseReceived); ok {
-
-			// Ignore non XHR responses
-			if event.Type != "XHR" {
-				return
-			}
-
-			go func() {
-				// Get response body
-				c := chromedp.FromContext(ctx)
-				rbp := network.GetResponseBody(event.RequestID)
-				body, err := rbp.Do(cdp.WithExecutor(ctx, c.Target))
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				// Check XHR response body for correct data
-				if strings.HasPrefix(string(body), `{"account_created"`) {
-
-					// Parse JSON data
-					json.Unmarshal([]byte(body), &parseInstagramResponse)
-					if parseInstagramResponse.Errors.Username != nil {
-						fmt.Printf("Instagram: username %s is NOT available\n", username)
-						InstagramAvailable = false
-					} else {
-						fmt.Printf("Instagram: username %s is available\n", username)
-						InstagramAvailable = true
-					}
-				}
-			}()
-		}
-	})
+func submit(urlstr, selector, username string) (chromedp.Tasks, error) {
 
 	return chromedp.Tasks{
-
-		network.Enable(),
 		chromedp.Navigate(urlstr),
+		// chromedp.Sleep(time.Second),
 		chromedp.WaitVisible(selector),
 		chromedp.SendKeys(selector, username),
-		chromedp.Click(`//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[7]/div/button`, chromedp.BySearch),
-		chromedp.Sleep(time.Second * 2),
+		// chromedp.Sleep(time.Second),
+		chromedp.WaitVisible(`//*[@id="react-root"]/section/main/div`),
+		// chromedp.Click(`//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[7]/div/button`, chromedp.BySearch),
+		chromedp.Click(`//*[@id="react-root"]/section/main/div`, chromedp.BySearch),
+		// chromedp.Sleep(time.Second),
+		chromedp.WaitVisible(`//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[5]/div/div/span`),
+		chromedp.Nodes(`//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[5]/div/div/span`, &nodes, chromedp.AtLeast(0)),
 	}, nil
 }
