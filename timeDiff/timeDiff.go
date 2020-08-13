9b0f8b8b7fa3a0b2421b8cb8a967ae4b1489fe1d
Oliver Proud <oliverproud@users.noreply.github.com>
2020-08-13 13:10:52 +0100

    clean up print outs

--- timeDiff/timeDiff.go
+++ timeDiff/timeDiff.go
@@ -8,15 +8,13 @@ import (
 // CalculateDiff calculates the elapsed time since a record was last checked
 func CalculateDiff(timestamp time.Time) {
 	timeDiff := time.Since(timestamp)
-	fmt.Printf("\nTime difference: %v\n", timeDiff)
+	fmt.Printf("Time difference: %v\n", timeDiff)
 
 	limit := time.Second * 43000
 
 	if timeDiff > limit {
 		fmt.Println("Time is greater than allowed")
-		fmt.Println()
 	} else {
 		fmt.Println("Time OK")
-		fmt.Println()
 	}
 }
